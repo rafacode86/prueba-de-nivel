3c5b7871d4e7d8ac98ef31bc6469e8293ba53194
Rafa Bello <rafabp986@outlook.com>
2025-07-09 15:37:14 +0200

    modificacion de funcion buscarCiudadMasGrande, cambio de foreach por for

--- funciones.php
+++ funciones.php
@@ -1,14 +1,15 @@
 <?php
 
     function buscarCiudadMasGrande(array $ciudades) {
-        $ciudadGrande= $ciudades[0];
-        foreach($ciudades as $ciudad) {
-            if ($ciudad->getSuperficie() > $ciudadGrande-> getSuperficie()) {
-                $ciudadGrande = $ciudad; 
+        $ciudadGrande = $ciudades[0];
+        for($i = 1; $i < count($ciudades); $i++) {
+            if ($ciudades[$i]->getSuperficie() > $ciudadGrande-> getSuperficie()) {
+                $ciudadGrande = $ciudades[$i]; 
             }
         }
         echo "la ciudad mas grande es {$ciudadGrande->getNombre()}\n";
-    }
+        }
+    
 
     function buscarCiudadesPartidoPolitico(array $ciudades, Partido $partido) {
         $orientacionPolitica = false;
